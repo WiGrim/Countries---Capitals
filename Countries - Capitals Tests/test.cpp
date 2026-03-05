98a4271a0ce1ba03ea4462b27b57cd61f1f0fa21
WiGrim <rassoha04@gmail.com>
2026-03-05 17:54:05 +0700

    Add load from file test

--- Countries - Capitals Tests/test.cpp
+++ Countries - Capitals Tests/test.cpp
@@ -6,3 +6,12 @@ TEST(GameTest, ClassCreation)
     Game game;
     ASSERT_NE(&game, nullptr);
 }
+
+TEST(GameTest, LoadPairsFromFile)
+{
+    Game game;
+
+    int result = game.loadFromFile("test_capitals.txt", "test_countries.txt");
+
+    ASSERT_EQ(0, result);
+}
